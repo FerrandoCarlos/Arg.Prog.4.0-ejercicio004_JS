8fdb9a538312384f873a6ba8b14667094afe6988
Ferrando_Carlos <cferrando2015@gmail.com>
2023-10-08 11:00:29 -0300

    Se linkea JS en html y se crea consigna

---
+++ js/ejercicio004.js
@@ -0,0 +1,3 @@
+/**
+ * Crear un array que tenga los siguientes colores: rojo, azul, verde, amarillo, naranja, marron, violeta, rosa. Recorrerlos con un FOR OF y mostrar los colores que comienzan con r y los que comienzan con v. 
+ */
