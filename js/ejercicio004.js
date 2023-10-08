d0a7e9ab7f726e2ea7f34b954177277a4ce42964
Ferrando_Carlos <cferrando2015@gmail.com>
2023-10-08 13:41:24 -0300

    ejercicio 4

--- js/ejercicio004.js
+++ js/ejercicio004.js
@@ -1,3 +1,11 @@
 /**
  * Crear un array que tenga los siguientes colores: rojo, azul, verde, amarillo, naranja, marron, violeta, rosa. Recorrerlos con un FOR OF y mostrar los colores que comienzan con r y los que comienzan con v. 
  */
+
+const colores = ["rojo","azul","verde","amarillo","naranja","marron","violeta","rosa"];
+
+for (const color of colores) {
+    if(color[0] === "r" || color[0]==="v"){
+        console.log(color);
+    }
+}
